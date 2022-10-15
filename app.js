8f6857b5e58e03838d3d31a462b3361980bb078b
Lisa Blomé <81302626+annaester@users.noreply.github.com>
2022-10-15 10:22:22 +0200

    set up the fetch

---
+++ app.js
@@ -0,0 +1,28 @@
+const cardbox = document.getElementById("cardbox");
+
+const API = "https://randomuser.me/api/?results=50";
+
+fetch(API)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("DATA!", data);
+
+    data.results.forEach((person) => {
+      console.log("person", person);
+
+      cardbox.innerHTML += `
+      <div>
+        <p>${person.name.first} ${person.name.last}</p>
+        <img src="${person.picture.medium}" alt="picture" />
+        <p>${person.location.city}</p>
+        <p>${person.email}</p>
+        <p>${person.phone}</p>
+      </div>
+      
+      `;
+    });
+  });
+
+// cardbox.innerHTML = data.forEach((person) => {
+//   console.log("person", person);
+// });
