15b9473dde426480fa6a82994992bced07b56f7f
Lisa Blomé <81302626+annaester@users.noreply.github.com>
2022-10-29 13:21:12 +0200

    added sorting and search function

--- app.js
+++ app.js
@@ -49,20 +49,17 @@ fetch("https://randomuser.me/api/?results=10")
 
       // THE SEARCHBAR FUNCTION
 
-      searchBar.addEventListener("input", (e) => {
-        // if (e.target.value === "") {
-        //   return;
-        // }
+      searchBar.addEventListener("keyup", (e) => {
+        const searchString = e.target.value.toLowerCase();
+        const filteredPeople = peopleArray.filter((person) => {
+          return (
+            person.name.first.toLowerCase().includes(searchString) ||
+            person.name.last.toLowerCase().includes(searchString)
+          );
+        });
 
-        // const searchResults = peopleArray.filter((person) => {
-        //   return person.name.first
-        //     .toLowerCase()
-        //     .includes(e.target.value.toLowerCase());
-        // });
-
-        //console.log("test3", peopleArray);
-
-        searchResults.forEach((person) => {
+        cardbox.innerHTML = "";
+        filteredPeople.forEach((person) => {
           cardbox.innerHTML += `
             <div class="cardliststyle">
             
@@ -89,11 +86,9 @@ fetch("https://randomuser.me/api/?results=10")
 // SORTING THE LIST
 
 sort.addEventListener("click", function () {
-  const newOrder = peopleArray.reverse();
-  console.log("annan ordning", peopleArray);
-
-  // cardbox.innerHTML = newOrder;
-  newOrder.forEach((person) => {
+  peopleArray.reverse();
+  cardbox.innerHTML = "";
+  peopleArray.forEach((person) => {
     cardbox.innerHTML += `
     <div class="cardliststyle">
     
@@ -134,35 +129,3 @@ function setList() {
     theme.setAttribute("href", "stylelist.css");
   }
 }
-
-// OOOOLD FETCH !
-
-const API = "https://randomuser.me/api/?results=50";
-
-fetch(API)
-  .then((res) => res.json())
-  .then((data) => {
-    //console.log("DATA!", data);
-    // data.results.forEach((person) => {
-    //   //console.log("person", person);
-    //   cardbox.innerHTML += `
-    //   <div class="cardliststyle">
-    //     <img class="profilepic" src="${person.picture.medium}" alt="picture" />
-    //   <div class="bg-white">
-    //     <div class="name-city">
-    //       <p class="name">${person.name.first} ${person.name.last}</p>
-    //       <p class="city">${person.location.city}</p>
-    //     </div>
-    //     <div class="phone-email">
-    //       <a class="email" href="mailto:${person.email}"> <i class="fa fa-envelope"></i></a>
-    //       <a class="phone" href="tel:${person.phone}"><i class="fa fa-phone"></i></a>
-    //     </div>
-    //     </div>
-    //   </div>
-    //   `;
-    // });
-    // const sort = document.getElementById("sort");
-    // sort.addEventListener("click", function () {
-    //   console.log("helloooo", data);
-    // });
-  });
